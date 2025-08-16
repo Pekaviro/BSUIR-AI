688ca94ed3568666993ff86622a5cedb47ee3228
pekaviro <139924578+Pekaviro@users.noreply.github.com>
2025-08-16 13:10:34 +0300

    Revert "Semester3"

--- PPOIS/lab_2/laba2/Lighting.cpp
+++
@@ -1,7 +0,0 @@
-#include "Lighting.h"
-
-Lighting::Lighting(const std::string& type) : type(type) {}
-
-void Lighting::Illuminate() {
-    std::cout << "Illuminating with: " << type << std::endl;
-}
--- PPOIS/lab_3/RainGauge.cpp
+++
@@ -1,9 +0,0 @@
-#include "RainGauge.h"
-
-void RainGauge::display() {
-    std::cout << "Precipitation: " << precipitation_ << " mm, Type: " << type_ << std::endl;
-}
-
-double RainGauge::getPrecipitation() const {
-    return precipitation_;
-}
